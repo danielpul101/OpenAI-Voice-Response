49b47c36e784dbbd65523a8346fdfe42211f5c45
danielpul101 <63880754+danielpul101@users.noreply.github.com>
2024-12-30 15:13:40 -0800

    Update createVoice.py

--- createVoice.py
+++ createVoice.py
@@ -5,10 +5,10 @@ import pickle
 from elevenlabs.client import ElevenLabs
 
 client = ElevenLabs(
-  api_key="d3062c42b47c5c88010b0a8a7af21188" # Defaults to ELEVEN_API_KEY
+  api_key="PUT YOURS HERE" # Defaults to ELEVEN_API_KEY
 )
 '''
-api_key="d3062c42b47c5c88010b0a8a7af21188"  # Eleven labs
+api_key="PUT YOURS HERE"  # Eleven labs
 client = Elevenlabs(api_key = api_key)
 '''
 try:
